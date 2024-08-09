c6f342aa1f20c0eeb769208f1e0db54a10029927
Andrew Gillies <andrew.gillies@gmail.com>
2024-08-08 22:27:51 -0700

    added env registration to prep for training with PPO

---
+++ double_pendulum_rl/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Cartpole balancing environment.
+"""
+
+import gymnasium as gym
+
+
+from omni.isaac.lab_tasks.manager_based.classic.cartpole import agents
+from .double_pendulum_env_cfg import DoublePendulumEnvCfg
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="Isaac-DoublePendulum-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": DoublePendulumEnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": agents.rsl_rl_ppo_cfg.CartpolePPORunnerCfg,
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
--- double_pendulum_rl/double_pendulum_env_cfg.py
+++ double_pendulum_rl/double_pendulum_env_cfg.py
@@ -22,7 +22,7 @@ import omni.isaac.lab_tasks.manager_based.classic.cartpole.mdp as mdp
 ##
 # Pre-defined configs
 ##
-from double_pendulum import DOUBLE_PENDULUM_CFG  # isort:skip
+from double_pendulum_rl.double_pendulum import DOUBLE_PENDULUM_CFG  # isort:skip
 
 
 ##
